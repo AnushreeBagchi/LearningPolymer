38c187d9bedb54c0ffe6ea0917400550cc8fa0cd
Anushree Bagchi <anushree.bagchi@Riversand.com>
2019-02-19 20:24:18 +0530

    First commit

--- icon-toggle.js
+++ icon-toggle.js
@@ -5,21 +5,44 @@ class IconToggle extends PolymerElement {
   static get template() {
     return html`
       <style>
-        /* shadow DOM styles go here */
-        span {
-          color: blue;
-        }
         :host {
           display: inline-block;
         }
+        iron-icon {
+          fill: var(--icon-toggle-color, rgba(0,0,0,0));
+          stroke: var(--icon-toggle-outline-color, currentcolor);
+        }
+        :host([pressed]) iron-icon {
+          fill: var(--icon-toggle-pressed-color, currentcolor);
+        }
       </style>
-  
+
       <!-- shadow DOM goes here -->
-      <span>Not much here yet.</span>
+      <iron-icon icon="[[toggleIcon]]"></iron-icon>
     `;
   }
+
+  static get properties() {
+    return {
+      toggleIcon: {
+        type: String
+      },
+      pressed: {
+        type: Boolean,
+        value: false,
+        notify: true,
+        reflectToAttribute: true
+      },
+    };
+  }
+
+  toggle() {
+    this.pressed = !this.pressed;
+  }
+
   constructor() {
     super();
+    this.addEventListener('click', this.toggle.bind(this));
   }
 }
 
